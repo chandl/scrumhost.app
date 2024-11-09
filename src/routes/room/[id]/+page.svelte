d7924c8d692d7dbb8dfb88fc1b212eade5e9c82a
Chandler <me@chandl.io>
2024-11-08 23:59:39 -0800

    formatting

--- src/routes/room/[id]/+page.svelte
+++ src/routes/room/[id]/+page.svelte
@@ -1,64 +1,70 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { Button } from '$lib/components/ui/button';
-	import { getRoom, getRoomParticipants, setActiveStory, setVotingFlag, subscribeToRoomUpdates, type RoomDetails } from '$lib/room';
+	import {
+		getRoom,
+		getRoomParticipants,
+		setActiveStory,
+		setVotingFlag,
+		subscribeToRoomUpdates,
+		type RoomDetails
+	} from '$lib/room';
 	import { createStory, getStoriesInRoom, subscribeToNewStories, type Story } from '$lib/story';
 	import { onMount } from 'svelte';
 
 	const roomId = $page.params.id;
-    let room: RoomDetails;
-    let participants: any [];
+	let room: RoomDetails;
+	let participants: any[];
 	let stories: Story[];
-    let activeStoryDetails: Story;
+	let activeStoryDetails: Story;
 
 	function handleCreateStory() {
 		createStory('Story Details Example', roomId);
 	}
 
-    function handleSetActiveStory(storyId: string) {
-        setActiveStory(roomId, storyId);
-    }
+	function handleSetActiveStory(storyId: string) {
+		setActiveStory(roomId, storyId);
+	}
 
-    function handleSetVotingFlag() {
-        if(!room){
-            return;
-        }
-        // Flip the flag
-        setVotingFlag(roomId, !room.is_voting_period)
-    }
+	function handleSetVotingFlag() {
+		if (!room) {
+			return;
+		}
+		// Flip the flag
+		setVotingFlag(roomId, !room.is_voting_period);
+	}
 
-    function setActiveStoryDetails(room: RoomDetails) {
-        if(!room.active_story_id) {
-            return;
-        }
+	function setActiveStoryDetails(room: RoomDetails) {
+		if (!room.active_story_id) {
+			return;
+		}
 
-        // Don't set active details if we can't find the story
-        let details = stories.find(story => story.id == room.active_story_id);
-        if (details) {
-            activeStoryDetails = details;
-        }
-    }
-
+		// Don't set active details if we can't find the story
+		let details = stories.find((story) => story.id == room.active_story_id);
+		if (details) {
+			activeStoryDetails = details;
+		}
+	}
 
 	onMount(async () => {
-        room = await getRoom(roomId);
-        stories = await getStoriesInRoom(roomId);
+		room = await getRoom(roomId);
+		stories = await getStoriesInRoom(roomId);
 
-        setActiveStoryDetails(room);
+		setActiveStoryDetails(room);
 
-        subscribeToRoomUpdates(roomId, record => {
-            console.log("Room Update Received", record);
-            room = record;
-            setActiveStoryDetails(room);
-        });
+		subscribeToRoomUpdates(roomId, (record) => {
+			console.log('Room Update Received', record);
+			room = record;
+			setActiveStoryDetails(room);
+		});
 
 		subscribeToNewStories(roomId, (record) => {
 			console.log('New Story Received:', record);
 			stories = [record, ...stories];
 		});
 
-        participants = await getRoomParticipants(roomId);
-        console.log("Participants", participants)
+		participants = await getRoomParticipants(roomId);
+		console.log('Participants', participants);
 	});
 </script>
 
@@ -71,7 +77,7 @@
 <h1>Point Values: {room?.point_values}</h1>
 <h1>Voting Now: {room?.is_voting_period}</h1>
 
-<Button on:click={handleSetVotingFlag }>Flip Voting Flag</Button>
+<Button on:click={handleSetVotingFlag}>Flip Voting Flag</Button>
 
 <Button on:click={handleCreateStory}>Create Story</Button>
 <br />
@@ -81,7 +87,7 @@
 	{#each participants as participant}
 		<li>
 			<strong>Name: {participant.name} - ID: {participant.id}</strong>
-        </li>
+		</li>
 	{/each}
 </ul>
 
@@ -90,7 +96,7 @@
 	{#each stories as story}
 		<li>
 			<strong>{story.details}</strong> - Completed: {story.completed} - {story.created} - {story.final_estimate}
-            <Button on:click={() => handleSetActiveStory(story.id)}>Set Active</Button>
-        </li>
+			<Button on:click={() => handleSetActiveStory(story.id)}>Set Active</Button>
+		</li>
 	{/each}
 </ul>
