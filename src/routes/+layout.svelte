8389b4269df2cc3c44aa953fc2e03b0fb36d5f52
Chandler <me@cseverson.com>
2024-11-11 21:29:36 -0800

    Add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import '../app.css';
 	import LogOutButton from './components/LogOutButton.svelte';
+	import { inject } from '@vercel/analytics'
 	let { children } = $props();
+
+	inject();
 </script>
 
 <LogOutButton />
