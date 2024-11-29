bbece0316cf66b4edbe0c2db198c64e4754a8af0
Chandler Severson <me@cseverson.com>
2024-11-29 14:42:25 -0800

    Revert "fix redirect on homepage"
    This reverts commit f2a31ce48264928d471fb6451f6aae9e90692071.

--- src/lib/scrum/user.ts
+++ src/lib/scrum/user.ts
@@ -55,6 +55,8 @@ export async function signup(name: string) {
 export async function validateLogin() {
 	// Check if the user is logged in by trying to get the current user
 	try {
+
+
 		try {
 			await pb.collection("users").authRefresh();
 		} catch (err) {
